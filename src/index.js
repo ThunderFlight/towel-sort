b426b2f1a7e736325de19680398f4ef9b1a5e2fa
ThunderFlight <igoreshkin2006@gmail.com>
2023-08-07 23:07:02 +0300

    feat: do task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+     let arr=[]
+     let count =0
+     if(matrix === undefined){
+          return []
+     }else{
+          for(let key of matrix){
+     count++
+       if(count%2===0){
+         key.reverse().map(index=>{arr.push(index)})
+       }else{
+          key.map(index=>arr.push(index))
+       }
+     
+         
+     }
+     return arr  
+     }
+  
 }
